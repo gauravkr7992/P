dda52390bf8af9dacf9c490ab8dfbe88425d63da
gauravkr7992 <gauravkr7992@gmail.com>
2023-08-12 10:47:21 +0530

    ...

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,45 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 int main(){
-string a="001";
-string b="010";
-string res;
-int x=a[0]^b[0];
-x=x^1;
-int y=a[1]^b[1];
-cout<<to_string(x)<<endl;
-res.insert(0,to_string(x));
-res.insert(0,to_string(y));
-cout<<res;
+vector<string> strs;
+strs.push_back("flower");
+strs.push_back("flow");
+strs.push_back("flo");
+cout<<strs[0][4];
+int siz=INT_MAX;
+        string prefix="";
+        for(int i=0;i<strs.size();i++){
+            string s=strs[i];
+            int l=s.size();
+            siz=min(siz,l);
+        }
+        
+        int count=1;
+        for(int i=0;i<siz;i++){
+            
+            char f='\0';
+            for(int j=0;j<strs.size();j++){
+                if(f=='\0'){
+                string s=strs[j];
+                f=s[i];
+                
+                }
+                else{
+                    string s=strs[j];
+                    if(s[i]!=f) {
+                        count=0;
+                        break;
+
+                }
+                
+            }
+            }
+           
+            if(count==0) break;
+            prefix.push_back(f);
+        }
+        cout<<prefix<<endl;
 return 0;
 }
